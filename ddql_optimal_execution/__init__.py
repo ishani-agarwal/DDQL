c4aa8147fa2f365298169e63ebe31fa0e0690c80
Ishani Agarwal <ishaniagarwal@Ishanis-MacBook-Pro.local>
2026-06-10 21:17:52 -0700

    Add risk-neutral environment copy and ran it on healthcare data

--- ddql_optimal_execution/__init__.py
+++ ddql_optimal_execution/__init__.py
@@ -3,12 +3,13 @@ from ._utils import get_device
 from .preprocessing import Preprocessor
 from .experience_replay._experience_replay import ExperienceReplay, ExperienceDict
 from .agent import TWAP, DDQL
-from .environnement import MarketEnvironnement
+from .environnement import MarketEnvironnement, MarketEnvironnementOld
 from .trainer import Trainer
 
 __all__ = [
     "DDQL",
     "MarketEnvironnement",
+    "MarketEnvironnementOld",
     "State",
     "StateArray",
     "ExperienceReplay",
--- ddql_optimal_execution/environnement/__init__.py
+++ ddql_optimal_execution/environnement/__init__.py
@@ -1,3 +1,4 @@
 from ._env import MarketEnvironnement
+from ._env_old import MarketEnvironnement as MarketEnvironnementOld
 
 __all__ = ["MarketEnvironnement"]
